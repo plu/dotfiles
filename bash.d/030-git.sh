736958977268792e06bc5e88fb3870cdf7858d7b
Johannes Plunien <johannes.plunien@xing.com>
2012-03-03 16:52:58 +0100

    Silence output.

--- bash.d/030-git.sh
+++ bash.d/030-git.sh
@@ -5,4 +5,4 @@ function parse_git_branch {
   git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/[\1$(parse_git_dirty)]/"
 }
 
-which hub && eval $(hub alias -s bash)
+which -s hub && eval $(hub alias -s bash)
