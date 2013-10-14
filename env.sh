40009a8733ff09a4a3c60b2fd3bb07fc4f3eb9c4
Johannes Plunien <johannes.plunien@xing.com>
2013-10-14 20:19:07 +0200

    Always use AppEngine's go.

--- env.sh
+++ env.sh
@@ -10,7 +10,7 @@ if [[ "$OS" == "Darwin" ]]; then
   export COPY_EXTENDED_ATTRIBUTES_DISABLE=true
   export JAVA_HOME=/Library/Java/Home
   export LSOPTIONS='-G'
-  export PATH=/usr/local/mysql/bin:/usr/local/bin:/usr/local/share/git/contrib:/opt/appengine-java-sdk/bin:/opt/go_appengine_1.8.3:$PATH
+  export PATH=/opt/go_appengine_1.8.3:/usr/local/mysql/bin:/usr/local/bin:/usr/local/share/git/contrib:/opt/appengine-java-sdk/bin:$PATH
   export IOS_SDK_VERSION=7.0
   export TEST_SCHEME=Smoke
   export DYLD_LIBRARY_PATH=/usr/local/mysql/lib
