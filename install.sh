a978dd72c511a5cb4ad3368d2fd43f14c279ddfa
Johannes Plunien <johannes.plunien@metaquark.de>
2013-12-28 21:39:58 +0100

    Let dotfiles installer be more aggressive.

--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 for file in $(ls $HOME/.dotfiles/); do
-    if [ ! -e "$HOME/.$file" ]; then
-        ln -sf $HOME/.dotfiles/$file $HOME/.$file
-    fi
+    ln -sf $HOME/.dotfiles/$file $HOME/.$file
 done
