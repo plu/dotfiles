bbce12b8fd915fc1b6d882b41cf9dafcaac51871
Johannes Plunien <johannes.plunien@metaquark.de>
2013-12-28 22:17:58 +0100

    Speed things up.

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 for file in $(ls $HOME/.dotfiles/); do
-    if [ "$(readlink $HOME/.$file)" == "$HOME/.dotfiles/$file" ]; then
+    if [ -L "$HOME/.$file" ]; then
         continue
     fi
     ln -svf $HOME/.dotfiles/$file $HOME/.$file
