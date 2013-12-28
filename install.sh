42e396a56960376f20529bb3498d7bfc316afb13
Johannes Plunien <johannes.plunien@metaquark.de>
2013-12-28 22:15:54 +0100

    Skip links if they are there already.

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 for file in $(ls $HOME/.dotfiles/); do
-    ln -sf $HOME/.dotfiles/$file $HOME/.$file
+    if [ "$(readlink $HOME/.$file)" == "$HOME/.dotfiles/$file" ]; then
+        continue
+    fi
+    ln -svf $HOME/.dotfiles/$file $HOME/.$file
 done
