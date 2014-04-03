d87bfab2db3fa3f299720c98d11eefa3bf9d2c7b
Johannes Plunien <johannes.plunien@metaquark.de>
2014-04-03 10:32:33 +0200

    Update PATH.

--- env.sh
+++ env.sh
@@ -12,6 +12,7 @@ if [[ "$OS" == "Darwin" ]]; then
   export JAVA_HOME=/Library/Java/Home
   export LSOPTIONS='-G'
   export PATH=/usr/local/heroku/bin:/opt/go_appengine:/usr/local/mysql/bin:/usr/local/bin:/usr/local/share/git/contrib:/opt/appengine-java-sdk/bin:/Applications/VMware\ Fusion.app/Contents/Library:$PATH
+  export PATH=/Applications/android-sdk-macos/sdk/platform-tools:/Applications/android-sdk-macos/sdk/tools:$PATH
   export IOS_SDK_VERSION=7.0
   export TEST_SCHEME=Smoke
   export DYLD_LIBRARY_PATH=/usr/local/mysql/lib
