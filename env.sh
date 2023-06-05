d30005f973a1d9c0ad77a57d4733297960f6f7ca
Johannes Plunien <johannes@plunien.com>
2023-06-05 08:55:50 +0200

    Eval path_helper

--- env.sh
+++ env.sh
@@ -88,6 +88,7 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 [[ -s "$NVM_DIR/bash_completion" ]] && \. "$NVM_DIR/bash_completion"
 [[ -s "$NVM_DIR/nvm.sh" ]] && \. "$NVM_DIR/nvm.sh"
 [[ -f ~/.fzf.zsh ]] && source ~/.fzf.zsh
+[[ -x /usr/libexec/path_helper ]] && eval `/usr/libexec/path_helper -s`
 
 which bat >/dev/null && alias cat='bat'
 which direnv >/dev/null && eval "$(direnv hook zsh)"
